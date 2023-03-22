decdd82872f30b8a4b38c34f85dfef571ddb902e
Tri Nguyen <ntri1805@gmail.com>
2023-03-21 20:33:03 -0400

    Latest

--- backtesting.py
+++ backtesting.py
@@ -1,9 +1,14 @@
 import backtrader as bt
-
+import pandas as pd
 class MA(bt.Strategy):
     def __init__(self):
         self.ma_100 = bt.indicators.MovingAverageSimple(self.data,period=20,
          plotname="20 SMA")
+
+df = pd.read_csv("TradingFiles/prices_round_1_day_-1.csv")
+#print(df.head(20))
+
+df[["Banana"]]
 
 
         
