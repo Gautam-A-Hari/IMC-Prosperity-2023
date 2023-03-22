86784e0aae70cdd4fd000d1bb27bf8ac0a2ee4ec
Tri Nguyen <ntri1805@gmail.com>
2023-03-21 21:29:21 -0400

    Update backtesting.py

--- backtesting.py
+++ backtesting.py
@@ -18,9 +18,5 @@ pd.set_option("display.max_columns", None)
 df_bananas = df.loc[df['product']=="BANANAS"]
 df_pearls = df.loc[df['product']=="PEARLS"]
 
-=======
-print(dataFrame.product)
->>>>>>> e3d0003d225bd3254ad6d2ea672508fc8a42a499
-
 
         
