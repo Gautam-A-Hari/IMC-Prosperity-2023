a52df122c0fc93dccba1995469f40ca6e0acbbe7
Sparsh N <sparsh.nair@hotmail.com>
2023-03-22 17:06:16 -0400

    updated trading data

--- TradingFiles/DataCollector.py
+++ TradingFiles/DataCollector.py
@@ -22,51 +22,45 @@ class DataCollector():
         df = self.readData()
         ProductNameFiltered = df[(df['Shipping Type'] == productName)]
 
-    def takePriceData():
-        df = pd.read_csv("TradingFiles/prices_round_1_day_-1.csv", sep=";")
+    def takePriceData(productName):
+        df = pd.read_csv("TradingFiles\prices_round_2_day_-1.csv", sep=";")
 
         pd.set_option("display.max_columns", None)
         df = df.fillna(0)
         df_bananas = df.loc[df['product']=="BANANAS"]
         df_pearls = df.loc[df['product']=="PEARLS"]
+        df_coconuts = df.loc[df['product']=="COCONUTS"]
+        df_pina_coladas = df.loc[df['product']=="PINA_COLADAS"]
 
-        df_bananas = df_bananas[["timestamp", 
+        df_output = df.loc[df['product']==productName]
+        df_output = df_output[["timestamp", 
         "product","bid_price_1", "bid_volume_1",
                                 "ask_price_1", "ask_volume_1",
                                 "mid_price", "profit_and_loss"]]
         bid_list = []
-        for bid in df_bananas["bid_price_1"]:
+        for bid in df_output["bid_price_1"]:
             bid_list.append(bid)
 
         bid_vol_list = []
-        for bid_vol in df_bananas["bid_volume_1"]:
+        for bid_vol in df_output["bid_volume_1"]:
             bid_vol_list.append(bid_vol)
 
         ask_list = []
-        for ask in df_bananas["ask_price_1"]:
+        for ask in df_output["ask_price_1"]:
             ask_list.append(ask)
 
         ask_vol_list = []
-        for ask_vol in df_bananas["ask_volume_1"]:
+        for ask_vol in df_output["ask_volume_1"]:
             ask_vol_list.append(ask_vol)
 
-
         # print(bid_list[0])
-        df_bananas["pct_change"] = df["mid_price"].pct_change()
+        df_output["pct_change"] = df["mid_price"].pct_change()
         # print(df_bananas.head(50))
-        output = df_bananas.head(50)["mid_price"]
+        output = df_output.head(50)["mid_price"]
         print(output)
         return output
         
-        
-    
-    
     #def dataInFrameSize(self):
 # https://jmerle.github.io/imc-prosperity-visualizer/        
-        
-
-if __name__ == "__main__":
-    data = DataCollector(50, r"C:\Users\gauta\OneDrive - Indiana University\2022-2023\Spring term\Intro to software systems\Homework\IMC-Prosperity-2023\Round_1_data\prices_round_1_day_-2.csv")
-    print(data.readData())
     
         
--- TradingFiles/MovingVolatility.py
+++ TradingFiles/MovingVolatility.py
@@ -7,21 +7,21 @@ import numpy as np
 import pandas as pd
 
 volatilityArr = []
-priceWindow = DataCollector.takePriceData()
+priceWindow = DataCollector.takePriceData('COCONUT')
 
-class getMovingVolatility():
-    def getStdDev(self, priceWindow):
-        stdDev = np.std(priceWindow)
-        print(stdDev)
-        # volatility = self.getStdDev * np.sqrt(50)
-        return stdDev # , volatility
-    def getVolatility(self):
-        volatility = self.getStdDev * np.sqrt(50)
-        return volatility
-    def volatilityDataCollector(self):
-           self.volatilityArr = self.volatilityArr.append(self.getVolatility())
-           return volatilityArr
-    def makeDecision(self, priceWindow, takePriceData):
-        # returns true if a order needs to be bought, and false if no order needed
-		   if self.volatility not in range (self.volatilityArray(-1) - 0.2, self.volatilityArr(-2) + 0.2):
-                    return True
+# class getMovingVolatility():
+#     def getStdDev(self, priceWindow):
+#         stdDev = np.std(priceWindow)
+#         print(stdDev)
+#         # volatility = self.getStdDev * np.sqrt(50)
+#         return stdDev # , volatility
+#     def getVolatility(self):
+#         volatility = self.getStdDev * np.sqrt(50)
+#         return volatility
+#     def volatilityDataCollector(self):
+#            self.volatilityArr = self.volatilityArr.append(self.getVolatility())
+#            return volatilityArr
+#     def makeDecision(self, priceWindow, takePriceData):
+#         # returns true if a order needs to be bought, and false if no order needed
+# 		   if self.volatility not in range (self.volatilityArray(-1) - 0.2, self.volatilityArr(-2) + 0.2):
+#                     return True
