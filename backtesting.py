e3d0003d225bd3254ad6d2ea672508fc8a42a499
Gautam Hari <gautamhari@outlook.com>
2023-03-21 21:06:38 -0400

    updates to backtesting+datacollector

--- backtesting.py
+++ backtesting.py
@@ -1,14 +1,14 @@
-import backtrader as bt
+# import backtrader as bt
 import pandas as pd
-class MA(bt.Strategy):
-    def __init__(self):
-        self.ma_100 = bt.indicators.MovingAverageSimple(self.data,period=20,
-         plotname="20 SMA")
+#class MA(bt.Strategy):
+#    def __init__(self):
+#        self.ma_100 = bt.indicators.MovingAverageSimple(self.data,period=20,
+#         plotname="20 SMA")
+nameOfFile = "prices_round_1_day_-1.csv"
+dataFrame = pd.read_csv("TradingFiles\\"+nameOfFile, sep=";")
+print(dataFrame.head(20))
 
-df = pd.read_csv("TradingFiles/prices_round_1_day_-1.csv")
-#print(df.head(20))
-
-print(df.loc[:"Banana"])
+print(dataFrame.product)
 
 
         
