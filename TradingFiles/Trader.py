3f992f11d2de8e2a99de40d606747b1b3ead6016
Gautam Hari <gautamhari@outlook.com>
2023-03-23 21:18:45 -0400

    Made DataModelClass

--- TradingFiles/Trader.py
+++ TradingFiles/Trader.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-# from datamodel import OrderDepth, TradingState, Order
+from datamodel import OrderDepth, TradingState, Order
 from OrderDepth import *
 from TradingState import *
 from Order import *
@@ -96,13 +96,12 @@ def run(self, state: TradingState) -> Dict[str, List[Order]]:
 
                 # Define a fair value for the PEARLS.
                 # Note that this value of 1 is just a dummy value, you should likely change it!
-                best_ask = min(order_depth.sell_orders.keys())
-                best_bid = max(order_depth.buy_orders.keys())
-                acceptable_price = (best_ask + best_bid) / 2
 
                 # If statement checks if there are any SELL orders in the PEARLS market
                 if len(order_depth.sell_orders) > 0:
-
+                    best_ask = min(order_depth.sell_orders.keys())
+                    best_bid = max(order_depth.buy_orders.keys())
+                    acceptable_price = (best_ask + best_bid) / 2 
                     # Sort all the available sell orders by their price,
                     # and select only the sell order with the lowest price
                     best_ask = min(order_depth.sell_orders.keys())
@@ -145,13 +144,12 @@ def run(self, state: TradingState) -> Dict[str, List[Order]]:
 
                 # Define a fair value for the PEARLS.
                 # Note that this value of 1 is just a dummy value, you should likely change it!
-                best_ask = min(order_depth.sell_orders.keys())
-                best_bid = max(order_depth.buy_orders.keys())
-                acceptable_price = (best_ask + best_bid) / 2
 
                 # If statement checks if there are any SELL orders in the PEARLS market
                 if len(order_depth.sell_orders) > 0:
-
+                    best_ask = min(order_depth.sell_orders.keys())
+                    best_bid = max(order_depth.buy_orders.keys())
+                    acceptable_price = (best_ask + best_bid) / 2
                     # Sort all the available sell orders by their price,
                     # and select only the sell order with the lowest price
                     best_ask = min(order_depth.sell_orders.keys())
