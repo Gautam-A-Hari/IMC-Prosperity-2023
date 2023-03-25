d878431f8bf7a156be8ef23e1b39dd974fed50a0
Sparsh N <sparsh.nair@hotmail.com>
2023-03-25 17:11:48 -0400

    updated trader

--- TradingFiles/Trader3.py
+++ TradingFiles/Trader3.py
@@ -1,16 +1,34 @@
 import json
-from datamodel import OrderDepth, TradingState, Order
+from datamodel import Order, ProsperityEncoder, Symbol, TradingState
 from typing import Any
 
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]]) -> None:
+        print(json.dumps({
+            "state": state,
+            "orders": orders,
+            "logs": self.logs,
+        }, cls=ProsperityEncoder, separators=(",", ":"), sort_keys=True))
+
+        self.logs = ""
+
+logger = Logger()
+
 class Trader:
-    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+    def run(self, state: TradingState) -> dict[str, list[Order]]:
         """
         Only method required. It takes all buy and sell orders for all symbols as an input,
         and outputs a list of orders to be sent
         """
         # Initialize the method output dict as an empty dict
-        result = {}
-
+        orders1 = {}
+        coconuts_price = []
         # Iterate over all the keys (the available products) contained in the order depths
         for product in state.order_depths.keys():
             # Retrieve the Order Depth containing all the market BUY and SELL orders for COCONUT
@@ -21,7 +39,9 @@ class Trader:
             if len(order_depth.sell_orders) > 0:
                 best_ask = min(order_depth.sell_orders.keys())
                 acceptable_price = best_ask * 1.01
-                best_ask_volume = order_depth.sell_orders[best_ask]
+                if product == 'COCONUTS':
+                    logger.print("COCONUTS")
+                    coconuts_price.append(best_ask)
                 # Check if the lowest ask (sell order) is lower than the above defined fair value
                 if best_ask < acceptable_price:
                     # In case the lowest ask is lower than our fair value,
@@ -29,23 +49,23 @@ class Trader:
                     # The code below therefore sends a BUY order at the price level of the ask,
                     # with the same quantity
                     # We expect this order to trade with the sell order
-                    print("BUY", str(-best_ask_volume) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_volume))
+                    logger.print("BUY", str(20) + "x", best_ask)
+                    orders.append(Order(product, best_ask, -20))
             # The below code block is similar to the one above,
             # the difference is that it finds the highest bid (buy order)
             # If the price of the order is higher than the fair value
             # This is an opportunity to sell at a premium
             if len(order_depth.buy_orders) > 0:
                 best_bid = max(order_depth.buy_orders.keys())
-                best_bid_volume = order_depth.buy_orders[best_bid]
                 acceptable_price = best_bid * 0.99
                 if best_bid > acceptable_price:
-                    print("SELL", str(best_bid_volume) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_volume))
+                    logger.print("SELL", str(20) + "x", best_bid)
+                    orders.append(Order(product, best_bid, -20))
             # Add all the above orders to the result dict
-            result[product] = orders
+            orders1[product] = orders
 
             # Return the dict of orders
             # These possibly contain buy or sell orders for PEARLS
             # Depending on the logic above
-        return result
+        logger.flush(state, orders1)
+        return orders1
