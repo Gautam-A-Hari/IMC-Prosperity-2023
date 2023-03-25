b86ef040b4c170dee76aea7cc7da680bd5781809
Sparsh N <sparsh.nair@hotmail.com>
2023-03-25 18:11:37 -0400

    submit

--- TradingFiles/Trader3.py
+++ TradingFiles/Trader3.py
@@ -1,26 +1,27 @@
-import json
-from datamodel import Order, ProsperityEncoder, Symbol, TradingState
+# import json
+from datamodel import Order, Symbol, TradingState
 from typing import Any
 
-class Logger:
-    def __init__(self) -> None:
-        self.logs = ""
+# class Logger:
+#     def __init__(self) -> None:
+#         self.logs = ""
 
-    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
-        self.logs += sep.join(map(str, objects)) + end
+#     def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+#         self.logs += sep.join(map(str, objects)) + end
 
-    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]]) -> None:
-        print(json.dumps({
-            "state": state,
-            "orders": orders,
-            "logs": self.logs,
-        }, cls=ProsperityEncoder, separators=(",", ":"), sort_keys=True))
+#     def flush(self, state: TradingState, orders: dict[Symbol, list[Order]]) -> None:
+#         print(json.dumps({
+#             "state": state,
+#             "orders": orders,
+#             "logs": self.logs,
+#         }, cls=ProsperityEncoder, separators=(",", ":"), sort_keys=True))
 
-        self.logs = ""
+#         self.logs = ""
 
-logger = Logger()
+# logger = Logger()
 
 class Trader:
+    coconuts_price = []
     def run(self, state: TradingState) -> dict[str, list[Order]]:
         """
         Only method required. It takes all buy and sell orders for all symbols as an input,
@@ -28,7 +29,7 @@ class Trader:
         """
         # Initialize the method output dict as an empty dict
         orders1 = {}
-        coconuts_price = []
+
         # Iterate over all the keys (the available products) contained in the order depths
         for product in state.order_depths.keys():
             # Retrieve the Order Depth containing all the market BUY and SELL orders for COCONUT
@@ -40,8 +41,7 @@ class Trader:
                 best_ask = min(order_depth.sell_orders.keys())
                 acceptable_price = best_ask * 1.01
                 if product == 'COCONUTS':
-                    logger.print("COCONUTS")
-                    coconuts_price.append(best_ask)
+                    coconuts_price = self.coconuts_price.append(best_ask)
                 # Check if the lowest ask (sell order) is lower than the above defined fair value
                 if best_ask < acceptable_price:
                     # In case the lowest ask is lower than our fair value,
@@ -49,7 +49,8 @@ class Trader:
                     # The code below therefore sends a BUY order at the price level of the ask,
                     # with the same quantity
                     # We expect this order to trade with the sell order
-                    logger.print("BUY", str(20) + "x", best_ask)
+                    #logger.print("BUY", str(20) + "x", best_ask)
+                    print("BUY", str(20) + "x", best_ask)
                     orders.append(Order(product, best_ask, -20))
             # The below code block is similar to the one above,
             # the difference is that it finds the highest bid (buy order)
@@ -59,7 +60,8 @@ class Trader:
                 best_bid = max(order_depth.buy_orders.keys())
                 acceptable_price = best_bid * 0.99
                 if best_bid > acceptable_price:
-                    logger.print("SELL", str(20) + "x", best_bid)
+                    #logger.print("SELL", str(20) + "x", best_bid)
+                    print("SELL", str(20) + "x", best_bid)
                     orders.append(Order(product, best_bid, -20))
             # Add all the above orders to the result dict
             orders1[product] = orders
@@ -67,5 +69,6 @@ class Trader:
             # Return the dict of orders
             # These possibly contain buy or sell orders for PEARLS
             # Depending on the logic above
-        logger.flush(state, orders1)
-        return orders1
+        #logger.print(coconuts_price)
+        #logger.flush(state, orders1)
+        return orders1, coconuts_price
